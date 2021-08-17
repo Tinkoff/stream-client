52285165a3f68ec9adf38c55cc024f385c904506
ivovk <adikue@gmail.com>
2021-08-17 12:41:39 +0300

    Clear the branch

--- ci/travis-osx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-brew unlink boost
-brew install ./boost@1.66--1.66.0.catalina.bottle.tar.gz
--- include/stream-client/stream-client.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stream-client/connector/connection_pool.hpp>
-#include <stream-client/connector/connector.hpp>
-#include <stream-client/resolver/resolver.hpp>
-#include <stream-client/stream/dgram_socket.hpp>
-#include <stream-client/stream/http_socket.hpp>
-#include <stream-client/stream/ssl_stream_socket.hpp>
-#include <stream-client/stream/stream_socket.hpp>
